5c2123bfd6fbdaa80ac4f3bf9675dda0a316001c
chenwj <chenwj@raincom.cn>
2025-10-14 15:42:26 +0800

    Test rviz draw vehicle

--- a_star/main.cpp
+++ a_star/main.cpp
@@ -13,7 +13,18 @@ int main()
     mg.add_obstacle(2, 2, 1, 1);
     to_rviz_map(mg.map, rviz_map);
 
-    viz->draw_gridmap2d("test", rviz_map);
+    rviz::VehState2d state;
+    state.x = 10;
+    state.y = 2;
+    state.heading = 0.5f;
+    state.steer_angle = 0.2f;
+    state.wheel_radius = 0.4f;
+    state.wheel_base = 2.8f;
+    state.wheel_track = 1.9f;
+    state.wheel_width = 0.3f;
+
+    viz->draw_gridmap2d("test/map", rviz_map);
+    viz->draw_vehicle2d("test/veh", state);
     while (!viz->closed()) {
         viz->render();
     }
